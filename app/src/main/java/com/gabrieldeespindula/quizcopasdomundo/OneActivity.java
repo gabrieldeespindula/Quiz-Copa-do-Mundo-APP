8f08400482e1f06686c2df5efab84e9cbbf73ffd
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-04 13:55:55 -0300

    Mapping buttons.

--- app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/OneActivity.java
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/OneActivity.java
@@ -2,12 +2,24 @@ package com.gabrieldeespindula.quizcopasdomundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OneActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+        this.mViewHolder.trueButton = findViewById(R.id.true_button);
+        this.mViewHolder.trueButton = findViewById(R.id.false_button);
+
+    }
+
+    private static class ViewHolder{
+        Button trueButton;
+        Button falseButton;
     }
 }
