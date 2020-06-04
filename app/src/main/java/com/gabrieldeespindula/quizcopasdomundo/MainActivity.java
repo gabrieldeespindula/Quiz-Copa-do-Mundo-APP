8eb4a5d55a26f9bf6767ed3fab555a53355d3291
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-04 13:01:47 -0300

    Mapping interface elements through the ViewHolder.

--- app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/MainActivity.java
@@ -2,12 +2,24 @@ package com.gabrieldeespindula.quizcopasdomundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.buttonStart = findViewById(R.id.button_start);
+        this.mViewHolder.buttonStart = findViewById(R.id.button_credits);
+
+    }
+
+    private static class ViewHolder {
+        Button buttonStart;
+        Button buttonCredits;
     }
 }
