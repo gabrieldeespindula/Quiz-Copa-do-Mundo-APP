6af26893ddf3f5e9af190c751cb00f6f96f5ac45
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-04 13:42:32 -0300

    Adding function to the buttons.

--- app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.gabrieldeespindula.quizcopasdomundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,16 +17,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         this.mViewHolder.buttonStart = findViewById(R.id.button_start);
-        this.mViewHolder.buttonStart = findViewById(R.id.button_credits);
+        this.mViewHolder.buttonCredits = findViewById(R.id.button_credits);
 
-        this.mViewHolder.buttonStart.setOnClickListener();
-        this.mViewHolder.buttonCredits.setOnClickListener();
-
+        this.mViewHolder.buttonStart.setOnClickListener(this);
+        this.mViewHolder.buttonCredits.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        if (v.getId() == R.id.button_start){
+            Intent question_one = new Intent(this, OneActivity.class);
+            startActivity(question_one);
+        } else if (v.getId() == R.id.button_credits){
+            Toast.makeText(this, "Just Gabriel de Espindula", Toast.LENGTH_LONG).show();
+        }
     }
 
     private static class ViewHolder {
