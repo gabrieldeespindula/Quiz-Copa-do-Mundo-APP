72f0dbe936b97ae11f8fcd1741e2c931982c4239
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-04 14:16:33 -0300

    Creating the functions of the true and false buttons.

--- app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/OneActivity.java
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/OneActivity.java
@@ -1,9 +1,11 @@
 package com.gabrieldeespindula.quizcopasdomundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class OneActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,6 +26,14 @@ public class OneActivity extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
+        Intent questiontwo = new Intent(this, TwoActivity.class);
+        if (v.getId() == R.id.true_button) {
+            questiontwo.putExtra("correct", 1);
+            Toast.makeText(this,"Right!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this,"Wrong!", Toast.LENGTH_SHORT).show();
+        }
+        startActivity(questiontwo);
 
     }
 
