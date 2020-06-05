b64bc0e1c417658061dcd2213eb89dba4d138f15
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-05 12:40:45 -0300

    TwoActivity.java equals OneActivity.java.

--- app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/TwoActivity.java
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/TwoActivity.java
@@ -1,13 +1,46 @@
 package com.gabrieldeespindula.quizcopasdomundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class TwoActivity extends AppCompatActivity {
+public class TwoActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_two);
+        setContentView(R.layout.activity_one);
+
+        this.mViewHolder.trueButton = findViewById(R.id.true_button);
+        this.mViewHolder.falseButton = findViewById(R.id.false_button);
+
+        this.mViewHolder.trueButton.setOnClickListener(this);
+        this.mViewHolder.falseButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent questiontwo = new Intent(this, TwoActivity.class);
+        if (v.getId() == R.id.true_button) {
+            questiontwo.putExtra("correct", 1);
+            Toast.makeText(this,"Right!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this,"Wrong!", Toast.LENGTH_SHORT).show();
+        }
+        startActivity(questiontwo);
+
+    }
+
+    private static class ViewHolder{
+        Button trueButton;
+        Button falseButton;
     }
 }
+
