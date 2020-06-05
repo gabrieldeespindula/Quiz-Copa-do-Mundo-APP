6462a468112857fc3b01e9956704ca33e1bde9c8
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-05 13:12:00 -0300

    Creating CorrectConstants

---
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/CorrectConstants.java
@@ -0,0 +1,10 @@
+package com.gabrieldeespindula.quizcopasdomundo;
+
+public class CorrectConstants {
+    public static int one = 0;
+    public static int two = 0;
+    public static int three = 0;
+    public static int four = 0;
+    public static int five = 0;
+
+}
