80d75bb325180fb4ac969d7488be4d97ab16862b
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-05 13:40:25 -0300

    ThreeActivity is ready!

--- app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/ThreeActivity.java
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/ThreeActivity.java
@@ -1,13 +1,47 @@
 package com.gabrieldeespindula.quizcopasdomundo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ThreeActivity extends AppCompatActivity {
+public class ThreeActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+
+
+        this.mViewHolder.trueButton = findViewById(R.id.true_three);
+        this.mViewHolder.falseButton = findViewById(R.id.false_three);
+
+        this.mViewHolder.trueButton.setOnClickListener(this);
+        this.mViewHolder.falseButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent questionfour = new Intent(this, FourActivity.class);
+        if (v.getId() == R.id.true_three) {
+            Toast.makeText(this,"Wrong!", Toast.LENGTH_SHORT).show();
+        } else {
+            CorrectConstants.two = 1;
+            Toast.makeText(this,"Right!", Toast.LENGTH_SHORT).show();
+        }
+        startActivity(questionfour);
+
+    }
+
+    private static class ViewHolder{
+        Button trueButton;
+        Button falseButton;
     }
 }
+
