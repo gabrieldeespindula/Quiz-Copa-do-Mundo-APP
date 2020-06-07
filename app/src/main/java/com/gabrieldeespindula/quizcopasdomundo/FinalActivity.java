2e23ebb2578aa1ff5407df1a32d93bdb726315c0
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-07 10:13:32 -0300

    Mapping textResult.

--- app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/FinalActivity.java
+++ app/src/main/java/com/gabrieldeespindula/quizcopasdomundo/FinalActivity.java
@@ -2,12 +2,22 @@ package com.gabrieldeespindula.quizcopasdomundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FinalActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+
+        this.mViewHolder.textResult = findViewById(R.id.text_result);
+
+    }
+
+    private static class ViewHolder {
+        TextView textResult;
     }
 }
